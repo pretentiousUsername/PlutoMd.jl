d1c35ed68dd9611b3f732be156e50186d8e8e04a
pretentiousUsername <mitchell.ian.2001@gmail.com>
2023-03-18 18:34:00 -0400

    add docs in code

--- src/metadata.jl
+++ src/metadata.jl
@@ -1,3 +1,17 @@
+"""
+---
+title: metadata.jl
+author: Ian Mitchell
+license: BSD-3
+purpose: Extract metadata from a Pluto.jl notebook.
+---
+"""
+
+"""
+    deleteComment(line)
+
+Deletes the comments from an input string.
+"""
 function deleteComment(line::String)
     line = split(line)
 
@@ -16,7 +30,19 @@ function deleteComment(line::String)
 end
 
 
-function frontmatterToStrings(filename::String)
+"""
+I'm a bit iffy on this. I think I want to try opening and closing files as little
+as possible for vague aesthetic reasons. Maybe there's some added security in
+there?---do I care about security?
+"""
+
+
+"""
+    getFrontmatter(filename)
+
+Extract the frontmatter from a Pluto noteboke.
+"""
+function getFrontmatter(filename::String)
     open(filename, "r") do file
 
         lines = readlines(file)
@@ -39,8 +65,24 @@ function frontmatterToStrings(filename::String)
     end
 end
 
+"""
+    encloseInBlock(text, block)
 
-funny1 = "# OHHHH MY! OHHH! DUDE THE THING THAT EVERYBODY---! ONION!---OH UH I
+Enclose a metadata header within a block of characters.
+"""
+function encloseInBlock(text::String, block::String)
+    block = block * "\n"
+    return block * text * "\n" * block * "\n"
+end
+
+# I prefer YAML, but TOML is easier to make from the start. I'll do YAML later.
+function makeTomlMetadata(titleArray::Array{String})
+    block = "+++"
+    
+end
+
+
+#=funny1 = "# OHHHH MY! OHHH! DUDE THE THING THAT EVERYBODY---! ONION!---OH UH I
 GOT AN ONION RING!"
 
 funny2 = "#> chomp chomp chomp"
@@ -48,10 +90,10 @@ funny2 = "#> chomp chomp chomp"
 funny3 = "Brush your teeth if you want to not go to fucking jail kids."
 
 
-#=jerma = [funny1, funny2, funny3]
+jerma = [funny1, funny2, funny3]
 
 for funny in jerma
     println(deleteComment(funny))
 end=#
 
-#=println( deleteComment.( frontmatterToStrings("../testing/son.jl")  ) )=#
+println( deleteComment.( getFrontmatter("../testing/son.jl")  ) )
