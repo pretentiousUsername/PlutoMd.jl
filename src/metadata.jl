b8abc88a5caed9c69b9910e06c3dd64378f3ac14
pretentiousUsername <mitchell.ian.2001@gmail.com>
2023-03-17 22:19:16 -0400

    removed a lot of comments At first I started doing a bunch of literate programming, but after a while I found it was quite difficult keeping up with the comments. Rather than continue with all that literate programming stuff, I'll just focus on making some quality documentation.

--- src/metadata.jl
+++ src/metadata.jl
@@ -1,25 +1,5 @@
 using Markdown
 
-md"""
----
-title: metadata.jl
-author: Ian Mitchell
-purpose: Extract metadata from a pluto notebook and format it.
----
-
-There is a fair amount of work that needs to be done in order to turn a Pluto
-notebook into a pandoc markdown file, however, none of it is so hard that it's
-impossible---this is difficult enough to be challenging, but not so much that
-it's frustrating by programming standards.
-
-
-# Delete comments and strings
-Before we can move on to more fun document stuff, a fair amount of the legwork
-will need to be done by deleting comments and strings.
-
-## Delete comments
-"""
-
 function deleteComment(line::String)
     line = split(line)
 
@@ -30,47 +10,12 @@ function deleteComment(line::String)
         line = join( deleteat!(line, 1), " " )
         return line
 
-md"""
-Rather than return an error, I figured it would be better to return the
-original string back without any modifications---if this backfires, just swap
-the comments between the following lines.
-"""
     else
         return join(line, " ")
         #error("line is not commented.")
 
     end
 end
-
-md"""
-# Make metadata
-The first thing I want to do is take care of generating a YAML metadata block.
-This is the first challenge to take care of---you don't read many books that
-don't have titles on the cover, unless you're reading some weird new Dadaist
-book that's never been seen before.
-
-The first thing to do is to read the file until it comes across the metadata
-block. In order to do this, first the program skips down three lines, because
-the first three lines in a Pluto notebook are always
-
-```jl
-### A Pluto.jl notebook ###
-# v0.19.22
-
-```
-
-
-This means that for speed's sake, I don't really need to care about finding
-the frontmatter---all the program needs to do is skip down a few lines, then
-find the end of the front matter. In order to do that, the program just needs
-to find the last line with a `#>`, and then skip down a line after that.
-
-Now, let's get on with it.
-
-First, we'll create a function that makes the metadata an array of strings.
-
-"""
-
 
 
 function frontmatterToStrings(filename::String)
@@ -95,21 +40,6 @@ function frontmatterToStrings(filename::String)
    
     end
 end
-
-
-
-    
-    
-    
-
-md"""
-The front matter usually looks something like this
-```jl
-#> [frontmatter]
-#> title = bla bla bla
-#> author = foo
-```
-"""
 
 
 funny1 = "# OHHHH MY! OHHH! DUDE THE THING THAT EVERYBODY---! ONION!---OH UH I
