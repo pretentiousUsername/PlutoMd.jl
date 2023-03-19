14e968f14653833aff81796fbd8f8862500540c3
pretentiousUsername <mitchell.ian.2001@gmail.com>
2023-03-19 13:28:18 -0400

    add reference to original algorithm

--- src/boyerMoore.jl
+++ src/boyerMoore.jl
@@ -2,6 +2,9 @@
 ---
 title: boyerMoore.jl
 author: Ian Mitchell
-purpose: Find text in a file using the Boyer-Moore algorithm.
+purpose: Find text in a file using the Boyer-Moore algorithm
+(see https://www.cs.utexas.edu/~moore/publications/fstrpos.pdf).
 ---
 """
+
+
